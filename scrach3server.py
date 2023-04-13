16786af4d3f6df61e01c60b8ee9c3f37fef37acd
vivien mauriaucourt <vivien.mauriaucourt@supinfo.com>
2023-04-13 08:03:47 +0100

    v1 tours serveur

--- scrach3server.py
+++ scrach3server.py
@@ -9,7 +9,8 @@ hostname = socket.gethostname()
 host = socket.gethostbyname(hostname)
 # host = 'LocalHost'
 port = 45678
-
+client_list = []
+i = 0
 
 # ---------------------------------------------
 class ThreadClient(threading.Thread,):
@@ -19,6 +20,16 @@ class ThreadClient(threading.Thread,):
         self.connexion = c
         self.start()
         self.idc = idd
+        self.current_client = 0
+
+
+    def nextClient(self) -> None:
+        print(self.current_client < (len(connected) - 1), self.current_client, (len(connected) - 1))
+        if self.current_client < (len(connected) - 1):
+            self.current_client += 1
+        else:
+            self.current_client = 0
+
 
     def run(self):
         # recuperation du message d'un client
@@ -33,12 +44,24 @@ class ThreadClient(threading.Thread,):
                     # envoi du message à tous les clients
                     connected[0].send(msg1)
                     connected[1].send(msg1)
-                except:
-                    print("connection error")
+                    self.nextClient()
+                    msg2 = self.current_client
+                    print(msg2)
+
+
+                    connected[0].send(pickle.dumps(msg2))
+                    connected[1].send(pickle.dumps(msg2))
+
+
+
+                except Exception as e:
+                    print("connection error:")
+                    print(e)
                     connected.pop(self.idc)
                     global ide
                     ide -= 1
                     print(connected)
+
 
             #self.connexion.close()
 
@@ -69,6 +92,11 @@ ide = 0
 while 1:
     connexion, adresse = mySocket.accept()
     print("ide:",ide)
+
+    client_list.append(i)
+    print(i)
+    i += 1
+
     connected[ide] = connexion
     ThreadClient(connexion, ide)
 
@@ -78,3 +106,4 @@ while 1:
     print(msg)
     connexion.send(msg.encode("Utf8"))
     ide += 1
+
